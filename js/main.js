48683be7275cb47d1a4b0c00784ac062fc650e4d
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-04-25 18:35:52 +0300

    Сверстаны еще 2 экрана

--- js/main.js
+++ js/main.js
@@ -5,12 +5,29 @@ $(document).ready(function(){
     speed: 300,
     adaptiveHeight: true,
     customPaging : function(slider) {
-        return '<input type="radio" name="sl" class="slider-radio">';
+        return '<input type="radio" name="sl" class="slider-radio cases-slider-radio">';
       },
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: false
   });
 
-  $('.slider-radio:first').prop("checked", true);
+  $('.cases-slider-radio:first').prop("checked", true);
 });
+
+$(document).ready(function(){
+  $('.reviews-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    adaptiveHeight: true,
+    customPaging : function(slider) {
+        return '<input type="radio" name="sl" class="slider-radio reviews-slider-radio">';
+      },
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  });
+
+  $('.reviews-slider-radio:first').prop("checked", true);
+});s
