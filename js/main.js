af037e6671c614a6b7fe04522586ba63abc3fab6
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-04-26 15:55:16 +0300

    Свератн макет под 1920px

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.cases-slider').slick({
     dots: true,
     infinite: true,
     speed: 300,
     adaptiveHeight: true,
-    customPaging : function(slider) {
-        return '<input type="radio" name="sl" class="slider-radio cases-slider-radio">';
-      },
+    customPaging: function (slider) {
+      return '<input type="radio" name="cases-radio" class="cases-slider-radio">';
+    },
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: false
   });
 
   $('.cases-slider-radio:first').prop("checked", true);
-});
 
-$(document).ready(function(){
   $('.reviews-slider').slick({
     dots: true,
     infinite: true,
     speed: 300,
     adaptiveHeight: true,
-    customPaging : function(slider) {
-        return '<input type="radio" name="sl" class="slider-radio reviews-slider-radio">';
-      },
+    customPaging: function (slider) {
+      return '<input type="radio" name="reviews-radio" class="reviews-slider-radio">';
+    },
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false
+    arrows: true,
+    prevArrow: $('#prev-review'),
+    nextArrow: $('#next-review')
   });
 
   $('.reviews-slider-radio:first').prop("checked", true);
-});s
+});
